5585bcaff69303ff9d75b8f68749bed493b143ea
uditjangra <uditjangrax@gmail.com>
2025-12-03 15:12:43 +0530

    new question

---
+++ ProblemsSolved/arrays/ConcatenationofArray1929.java
@@ -0,0 +1,18 @@
+package arrays;
+
+public class ConcatenationofArray1929 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public int[] getConcatenation(int[] nums) {
+            int n = nums.length;
+            int[] ans = new int[n*2];
+            for(int i = 0; i < n; i++){
+                ans[i] = nums[i];
+                ans[n + i] = nums[i];
+            }
+            return ans;
+        }
+    }
+}
