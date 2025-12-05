8adbb706a6b1faef2ac77369793fd67b07cdbb61
uditjangra <uditjangrax@gmail.com>
2025-12-05 23:12:58 +0430

    init commit

---
+++ ProblemsSolved/arrays/Factorial2.java
@@ -0,0 +1,22 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class Factorial2 {
+    public static class Factorial {
+        public static void main(String[] args){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter a number:");
+            int input = scanner.nextInt();
+            System.out.printf("The factorial of %d is %d",input,factorial(input));
+        }
+        public static int factorial(int n){
+            int x = 1;
+            for(int i = 1; i <= n;i++){
+                x = x * i;
+            }
+            return x;
+        }
+    }
+
+}
