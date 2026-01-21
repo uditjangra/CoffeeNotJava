08a83eb2ec993347bc2f38ccb29b669233dede49
uditjangra <uditjangrax@gmail.com>
2026-01-21 21:41:52 +0530

    Searching in an Array using Linear Search

--- ProblemsSolved/KunalVideos/LinearSearchAlgorithms.java
+++ ProblemsSolved/KunalVideos/LinearSearchAlgorithms.java
@@ -10,18 +10,16 @@ public class LinearSearchAlgorithms {
             arr[i] = sc.nextInt();
         }
         int target = sc.nextInt();
-        System.out.println(search(arr,target));
+        int ans = search(arr,target);
+        System.out.println(ans);
     }
     //search in the array
     static int search(int[] array, int target){
-        if(array.length==0){
-            return -1;
-        }
-        for(int i = 0; i < array.length; i++){
-            if(target==array[i]){
-                return i;
+        for (int element : array) {
+            if (target == element) {
+                return element;
             }
         }
-        return 0;
+        return -1;
     }
 }
---
+++ ProblemsSolved/KunalVideos/Question1.java
@@ -0,0 +1,14 @@
+package KunalVideos;
+
+public class Question1 {
+    static void main() {
+        String name = "What is it?";
+        char target = 'i';
+
+    }
+    static boolean search(String a, char target){
+        if(a.length()==0){
+            return false;
+        }
+    }
+}
