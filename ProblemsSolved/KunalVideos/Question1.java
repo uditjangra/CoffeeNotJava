3fbc9a1603814357403cdbca39f34ffa5ffd00f1
uditjangra <uditjangrax@gmail.com>
2026-01-27 22:09:03 +0530

    Searching in an Array using Linear Search

--- ProblemsSolved/KunalVideos/Question1.java
+++ ProblemsSolved/KunalVideos/Question1.java
@@ -4,7 +4,6 @@ public class Question1 {
     static void main() {
         String name = "What is it?";
         char target = 'i';
-
     }
     static boolean search(String a, char target){
         if(a.length()==0){
